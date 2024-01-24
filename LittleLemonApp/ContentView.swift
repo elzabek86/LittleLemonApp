a6c65abbfda4b37d36cc50aa2da4823abbaa5daa
Jarek <jaroslaw-kaminski@outlook.com>
2024-01-24 22:54:29 +0000

    Background color

--- LittleLemonApp/ContentView.swift
+++ LittleLemonApp/ContentView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Color.green
+                .ignoresSafeArea()
+                
         }
-        .padding()
+        
+        
     }
+        
+    
+       
+        
+    
 }
 
 #Preview {
