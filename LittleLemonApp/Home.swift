b63c3cfb6e8094a9105b5ab1d4c043830fde50cd
Jarek <jaroslaw-kaminski@outlook.com>
2024-01-31 14:09:33 +0100

    Home, Menu, Onboarding Views. NavigationLink added. Initial style for registration form

---
+++ LittleLemonApp/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by Jarek  on 31/01/2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ LittleLemonApp/Menu.swift
@@ -0,0 +1,25 @@
+//
+//  Menu.swift
+//  LittleLemonApp
+//
+//  Created by Jarek  on 31/01/2024.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        VStack {
+            Text("Little Lemon")
+            Text("Chicago")
+            Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+            List {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    Menu()
+}
